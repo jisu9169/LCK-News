5d8ac6bfcb01c64b06cee4180f729c5d8a8acbce
hanraeul <toast17@naver.com>
2024-06-05 10:14:37 +0900

    init commit

---
+++ src/test/java/com/sparta/lck_news/LckNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.lck_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LckNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
